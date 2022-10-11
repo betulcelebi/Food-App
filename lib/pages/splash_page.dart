cf5e213db787ab18fc35f14cdb959ac459e50c1d
celebibetul <betulcelebi.05@gmail.com>
2022-10-11 13:12:22 +0300

    Splash page finished

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:food_app/widgets/account_text.dart';
+import 'package:food_app/widgets/bg_image.dart';
+import 'package:food_app/widgets/login_button.dart';
+import 'package:food_app/widgets/login_text.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,6 +16,16 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          BgImage(),
+          LoginText(),
+          LoginButton(),
+          AccountText(),
+        ],
+      ),
+    );
   }
 }
