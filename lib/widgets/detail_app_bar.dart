79f7d0314cf0cbcffae9ef0e93266ebda9f2c524
celebibetul <betulcelebi.05@gmail.com>
2022-10-14 10:51:32 +0300

    Detail appbar branch and widget created

---
+++ lib/widgets/detail_app_bar.dart
@@ -0,0 +1,36 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DetailAppBar extends StatelessWidget {
+  const DetailAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      actions: [
+        IconButton(
+            onPressed: () {},
+            icon: Container(
+                alignment: Alignment.center,
+                width: 32,
+                height: 32,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, color: Color(0xff80D9D9D9)),
+                child: Image.asset("assets/like.png")))
+      ],
+      leading: IconButton(
+          onPressed: () {},
+          icon: Container(
+              alignment: Alignment.center,
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle, color: Color(0xff80D9D9D9)),
+              child: Image.asset("assets/vector.png"))),
+    );
+  }
+}
